8940f1931a9d85ce87ea40af75b879601de6003c
MortorG <hugofustersanahuja@gmail.com>
2024-01-05 16:50:31 +0100

    Added strtrim

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hufuster <hufuster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 11:15:25 by hufuster          #+#    #+#             */
-/*   Updated: 2024/01/05 15:51:22 by hufuster         ###   ########.fr       */
+/*   Updated: 2024/01/05 16:01:18 by hufuster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,3 +44,4 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
